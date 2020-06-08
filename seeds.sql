7332c4f49b9948897d8be58ead57c2ddbde61aa2
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-08 16:26:12 -0400

    started seeds.sql file

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+-- Drops the employee_management_db if it exists currently --
+DROP DATABASE IF EXISTS employee_management_db;
+-- Creates the "employee_management_db" database --
+CREATE DATABASE employee_management_db;
