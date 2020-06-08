98a2025cd35bbd0e0af8ce44ce4af6ed61f29434
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-08 16:56:57 -0400

    changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('events').EventEmitter.prototype._maxListeners = 100;
 //Stores connection variables to mySQL (PLEASE INPUT PASSWORD)
 let connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 4000,
     user: "root",
     password: "[ENTER PASSWORD]",
     database: "employee_management_db"
