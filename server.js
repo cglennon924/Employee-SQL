44c156a57a04617e31e837843d23a4bf06ab880b
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-08 16:49:57 -0400

    inital connection

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
 const cTable = require("console.table")
+
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+//Stores connection variables to mySQL (PLEASE INPUT PASSWORD)
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "[ENTER PASSWORD]",
+    database: "employee_management_db"
+});
+  
