4b1c8d7dc9445566af966716a6cabbaef411270b
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-08 16:27:28 -0400

    added department table

--- seeds.sql
+++ seeds.sql
@@ -2,3 +2,13 @@
 DROP DATABASE IF EXISTS employee_management_db;
 -- Creates the "employee_management_db" database --
 CREATE DATABASE employee_management_db;
+
+-- Ensures which schema/database is used for the following SQL queries. 
+USE employee_management_db;
+
+-- Creates table for department 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT, 
+    department_name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+)
