e09a170708ee49b1eb6e3c637561671340b8fe3d
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-08 19:23:14 -0400

    added connect function

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ require('events').EventEmitter.prototype._maxListeners = 100;
 //Stores connection variables to mySQL (PLEASE INPUT PASSWORD)
 let connection = mysql.createConnection({
     host: "localhost",
-    port: 4000,
+    port: 3306,
     user: "root",
-    password: "[ENTER PASSWORD]",
+    password: "rootroot",
     database: "employee_management_db"
 });
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
   
